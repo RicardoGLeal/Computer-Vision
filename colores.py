1512e9b28b96d7a1f092bd3daad48b5e4ab3b046
Ricardo Glez Leal <ricardoglr664@gmail.com>
2021-09-15 12:36:18 -0500

    Se extraen los colores deseados y se aplica la máscara

--- colores.py
+++ colores.py
@@ -14,8 +14,15 @@ while capturaVideo.isOpened():
     
     #se obtienen los pixeles rgb y los convierte en hsv.
     hsv = cv2.cvtColor(camara, cv2.COLOR_BGR2HSV)
+    
+    #se definen los colores que se quieren extraer de la imagen.
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
 
-    cv2.imshow("En Vivo", hsv)
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    result = cv2.bitwise_and(camara, camara, mask=mask)
+
+    cv2.imshow("En Vivo", result)
 
     if cv2.waitKey(1) == ord("q"):
         break
