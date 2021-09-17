0ab0c55a65a292c59308ebbee8066f011d4bba2b
Carlo Angel Luján García <carlouxljlj@gmail.com>
2021-09-17 12:26:24 -0500

    Commented faces.py

--- Filtros/faces.py
+++ Filtros/faces.py
@@ -15,11 +15,15 @@ def run():
 		ret,frame = cap.read()
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-		faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+		#Se realiza la busqueda de caras en el video tomando escala 1.3 del clasficador
+		# y 5 neighbors juntos para saber si es una cara
+		faces = faceClassif.detectMultiScale(gray,1.3, 5)
 
+		#Se generan los rectangulos alrededor de la cara con color verde y ancho dos
 		for (x,y,w,h) in faces:
 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
 
+		#Se muestra el video en pantalla
 		cv2.imshow('frame',frame)
 		
 		if cv2.waitKey(1) & 0xFF == ord('q'):
